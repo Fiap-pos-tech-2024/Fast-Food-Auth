5a003eefdc197fae910c3f9ab30c5f177f889f4e
Michael Reis <michael_farias14@hotmail.com>
2025-03-16 17:13:43 -0300

    chore: method

--- src/adapters/lambdaHandler.js
+++ src/adapters/lambdaHandler.js
@@ -9,11 +9,7 @@ exports.handler = async (event) => {
   } else if (event.httpMethod === "POST" && event.path === "/login") {
     return await userController.login(event);
   } else if (event.httpMethod === "GET" && event.path === "/verify-token") {
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: "Token válido" }),
-    };
-    // return await userController.verifyToken(event);
+    return await userController.verifyToken(event);
   } else {
     return {
       statusCode: 404,
