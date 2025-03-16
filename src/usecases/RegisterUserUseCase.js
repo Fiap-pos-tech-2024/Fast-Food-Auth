852fb75d0d36d8bd038841f1a8d2e8861abeaf0c
Michael Reis <michael_farias14@hotmail.com>
2025-03-16 15:34:41 -0300

    chore: configure password

--- src/usecases/RegisterUserUseCase.js
+++ src/usecases/RegisterUserUseCase.js
@@ -3,7 +3,6 @@ const userRepository = require("../repositories/UserRepository");
 
 class RegisterUserUseCase {
   async execute({ cpf, password }) {
-    // Em produção, a senha deve ser armazenada com hash.
     const user = new User(cpf, password);
     return await userRepository.create(user);
   }
